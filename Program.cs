db1a5d7c5ef9081957b18b8f5bdc2676e69196bc
ShraddhaMallareddy <Shraddha.Mallareddy@cognizant.com>
2025-03-31 22:25:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,88 @@
+using Clubmates.web.Areas.Club.Services;
+using Clubmates.web.Models;
+using Clubmates.web.NewFolder;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+using System.Security.Claims;
+
+namespace Clubmates.web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddIdentity<ClubmatesUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+
+                options.Lockout= new LockoutOptions
+                {
+                    AllowedForNewUsers=true,
+                    DefaultLockoutTimeSpan=TimeSpan.FromMinutes(5),
+                    MaxFailedAccessAttempts=5
+                };
+                options.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<AppIdentityDbContext>()
+            .AddDefaultTokenProviders();
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppIdentityDbContext>(options=>
+            options
+            .UseSqlServer(
+                builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme= IdentityConstants.ApplicationScheme;
+            }).AddCookie();
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Account/Login";
+                options.AccessDeniedPath = "/Account/AccessDenied";
+            });
+            builder.Services.AddAuthorizationBuilder()
+                .AddPolicy("MustbeSuperAdmin", policy => 
+                                                policy
+                                                .RequireClaim(ClaimTypes.Role, "SuperAdmin")
+                                                .RequireClaim(ClaimTypes.Role, "Guest"))
+               .AddPolicy("MustbeGuest", policy => policy.RequireClaim(ClaimTypes.Role, "Guest"));
+
+            builder.Services.AddScoped<IClubLayoutService, ClubLayoutService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                     name: "Club",
+                     pattern: "Club/{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
